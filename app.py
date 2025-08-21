1a4b2aeee4ea111f78ed8c70e49a59d1e1517bd3
bajugo <texetbuy@gmail.com>
2025-08-21 22:59:43 +0900

    Update 22:59
    Немного побаловался. Видио закончилось на 6ой +- минуте

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+from unicodedata import digit
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
@@ -13,13 +15,20 @@ db = SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
-@app.route("/blog")
-def blog():
-    return render_template("blog.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route('/greet/<name>') # By this <name> we create a new variable
+def greet(name):
+    return f"hello, {name}"
+
+
+@app.route('/add/<int:number1>/<int:number2>') # We could write {int(number1) + int(number2)}, but this is easier
+def add(number1, number2):
+    return f"{number1} + {number2} = {number1 + number2}"
 
 
 if __name__ == "__main__":
