eb4cfe790a2f90d2f26a5178e6964380a4094d33
bajugo <texetbuy@gmail.com>
2025-08-22 21:14:58 +0900

    end 2 video

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
-from unicodedata import digit
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
@@ -10,25 +8,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 @app.route("/home")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/about")
+@app.route('/hello')
+def hello():
+    response = make_response('Hello world')
+    response.status_code = 202
+    response.headers['content-type'] = 'text/plain'
+    return response
+
+@app.route("/about", methods=['GET', 'POST'])
 def about():
     return render_template("about.html")
 
 
 @app.route('/greet/<name>') # By this <name> we create a new variable
 def greet(name):
-    return f"hello, {name}"
+    return f"hello, {name}" # If you use template, the curl request is large because it shows all the HTML code
 
 
 @app.route('/add/<int:number1>/<int:number2>') # We could write {int(number1) + int(number2)}, but this is easier
 def add(number1, number2):
     return f"{number1} + {number2} = {number1 + number2}"
+
+
+@app.route('/handle_url_params')
+def handle_params():
+    # if 'greeting' in request.args.keys() and 'name" in request.args.keys():
+    greeting = request.args['greeting']
+    name = request.args.get('name', 'guest')  # It is better to write args.get because it won't raise an error
+    return f"{name}, {greeting}"
+
 
 
 if __name__ == "__main__":
